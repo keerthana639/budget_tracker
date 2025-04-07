06204e237048bb5c477b0ad5020a73400be817a2
Keerthana Govardhana <71486650+keerthana639@users.noreply.github.com>
2025-04-07 20:55:20 +0200

    Update urls.py

--- budget_tracker/urls.py
+++ budget_tracker/urls.py
@@ -1,16 +1,11 @@
+# backend/budget_tracker/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from finances import views  # Import the views from your finances app
-from finances.views import LoginView,TransactionList, TransactionDetail, BudgetView, BudgetDetail, CategoryList, CategoryDetail, home
-
+from finances.views import home
 
 urlpatterns = [
-    path('', home, name='home'),  # Home endpoint
-    path('api/transactions/', TransactionList.as_view(), name='transaction-list'),  # List and create transactions
-    path('api/transactions/<int:pk>/', TransactionDetail.as_view(), name='transaction-detail'),  # Retrieve, update, delete a transaction
-    path('api/budgets/', BudgetView.as_view(), name='budget-list-create'),  # List and create budgets
-    path('api/budgets/<int:pk>/', BudgetDetail.as_view(), name='budget-detail'),  # Retrieve and update budget
-    path('api/categories/', CategoryList.as_view(), name='category-list-create'),  # List and create categories
-    path('api/categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),  # Retrieve, update, delete category
-    path('api/login/', LoginView.as_view(), name='login'),  # Add this line
+    path('admin/', admin.site.urls),        # Django admin interface
+    path('', home, name='home'),            # Home view
+    path('api/', include('finances.urls')), # Include all API routes from finances/urls.py
 ]
